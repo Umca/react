ceb1ec2f7814bd7abae28afad18e176e29fead3b
Olya <olga.akinshyna@gmail.com>
2018-01-23 23:36:38 +0200

    user sign up

--- server-app/router/api-router.js
+++ server-app/router/api-router.js
@@ -1,11 +1,21 @@
 const router = require('express').Router()
 const bodyParser = require('body-parser')
+const User = require('../models/user')
 
 const jsonParser = bodyParser.json()
 
 router.post('/signup', jsonParser, (req, res) => {
-    console.log(req.body)
-    res.send({"a": "1"})
+    User.findOne({ email: req.body.email }).then(response => {
+        if (response) {
+            res.send({message: 'We already have user with such email'})
+        } else {
+            new User(req.body)
+                .save()
+                .then(response => res.send({ "message": "New user added to db" }))
+            
+        }
+    })
+    
 })
 
 module.exports = router
