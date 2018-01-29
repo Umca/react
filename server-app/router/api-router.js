1dc4f1b619845786c8de99766acfa93401068b99
Olya <olga.akinshyna@gmail.com>
2018-01-29 23:15:03 +0200

    /login route

--- server-app/router/api-router.js
+++ server-app/router/api-router.js
@@ -1,3 +1,5 @@
+import { Error } from 'mongoose';
+
 const router = require('express').Router()
 const bodyParser = require('body-parser')
 const User = require('../models/user')
@@ -7,15 +9,32 @@ const jsonParser = bodyParser.json()
 router.post('/signup', jsonParser, (req, res) => {
     User.findOne({ email: req.body.email }).then(response => {
         if (response) {
-            res.send({message: 'We already have user with such email'})
+            res.send({ message: 'We already have user with such email' })
         } else {
             new User(req.body)
                 .save()
-                .then(response => res.send({ "message": "New user added to db" }))
-            
+                .then(respose => {
+                    res.send({ message: 'New user added' })
+                })
+
         }
     })
-    
+})
+
+router.post('/login', jsoParser, (req, res) => {
+    User.findOne({ email: req.body.email }).then(response => {
+        if (response) {
+            User.getAuthenticated(req.body.username, req.body.password, (err, user) => {
+                if (err) throw Error(err)
+
+                if (user) {
+                    res.send('new user added')
+                }
+            })
+        } else {
+            res.send('there is no user with such email')
+        }
+    })
 })
 
 module.exports = router
